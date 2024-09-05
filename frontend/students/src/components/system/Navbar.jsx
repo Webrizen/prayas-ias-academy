6500b5945274e3ea9bacc9e9551efefdd3369ac1
WebRizen <webrizen@gmail.com>
2024-09-05 20:01:28 +0530

    "Added SVG animation to globals.css and updated Navbar.jsx with new CSS class and z-index"

--- frontend/students/src/components/system/Navbar.jsx
+++ frontend/students/src/components/system/Navbar.jsx
@@ -43,7 +43,7 @@ export default function Navbar() {
     <>
       <header
         className="bg-[rgba(225,225,225,0.1)] backdrop-blur-2xl sticky top-0"
-        style={{ zIndex: "99" }}
+        style={{ zIndex: "9" }}
       >
         <div className="container mx-auto flex flex-row flex-wrap p-3 justify-between items-center">
           <Link
@@ -309,6 +309,7 @@ export default function Navbar() {
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
+                      className="svg-elem-1"
                       d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
                     />
                   </svg>
